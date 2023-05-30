8059335c91371f967f2f3bff29ce5474fe156df0
FelyppeRau <felypperau@gmail.com>
2023-05-30 12:21:15 +0100

    API Products Controller

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository; // inserir o "_" depois de inserir o field
+         
+        public ProductsController(IProductRepository productRepository) // ctrl. para inserir o field
+        {
+            _productRepository = productRepository; // inserir o "_" depois de inserir o field
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll()); // Busca todos os productos dentro do repositório e o (OK) embrulha tudo em Json
+        }
+    }
+}
